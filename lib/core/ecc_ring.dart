110c1aef445f471615276982f43178ebcd2b3ce0
Vyacheslav Trushkov <vyacheslav.trushkov@solarwinds.com>
2021-12-08 03:20:36 +0100

    attempt to implement linkable ring signature using ecc

--- lib/core/ecc_ring.dart
+++ lib/core/ecc_ring.dart
@@ -1,11 +1,9 @@
 import 'dart:convert';
-import 'dart:math';
 import 'dart:typed_data';
 
 import 'package:crypto/crypto.dart';
 import 'package:elliptic/elliptic.dart';
 import 'package:ninja_prime/ninja_prime.dart';
-import 'package:pointycastle/pointycastle.dart';
 
 class Ring {
   List<AffinePoint> publicKeys;
@@ -81,7 +79,7 @@ class Ring {
       ]);
     }
     signatures[myIndexKey] = (u - privateKey * challenges[myIndexKey]) % ec.n;
-    return([challenges[0], ...signatures, Y.X, Y.Y]);
+    return ([challenges[0], ...signatures, Y.X, Y.Y]);
   }
 
   BigInt H1(List<String> message) {
@@ -118,157 +116,225 @@ class Ring {
     while (!found) {
       x += BigInt.one;
       var fX = (x * x * x + BigInt.from(7)) % ec.p;
-      print(fX);
-      print(ec.p);
-      y = GFG().STonelli(fX, ec.p);
-      if (y != BigInt.from(-1)) {
+      try {
+        y = square_root_mod_prime(fX, ec.p);
         found = true;
+      } on Exception {
+        found = false;
       }
     }
     return AffinePoint.fromXY(x, y);
   }
-}
 
-class GFG {
-  BigInt z = BigInt.zero;
-
-// utility function to find
-// pow(base, exponent) % modulus
-  BigInt pow1(BigInt base1, BigInt exponent, BigInt modulus) {
-    BigInt result = BigInt.one;
-    base1 = base1 % modulus;
-    while (exponent > BigInt.zero) {
-      if (exponent % BigInt.two == BigInt.one) {
-        result = (result * base1) % modulus;
-      }
-      exponent = exponent >> 1;
-      base1 = (base1 * base1) % modulus;
+  BigInt jacobi(BigInt a, BigInt n) {
+    if (n < BigInt.from(3)) {
+      throw Exception(['jacobi']);
     }
-    return result;
-  }
+    if (n % BigInt.two == BigInt.one) {
+      throw Exception(['jacobi']);
+    }
+    a = a % n;
+    if (a == BigInt.zero) {
+      return BigInt.zero;
+    }
+    if (a == BigInt.one) {
+      return BigInt.one;
+    }
+    BigInt a1 = a;
+    BigInt e = BigInt.zero;
 
-// utility function to find gcd
-  BigInt gcd(BigInt a, BigInt b) {
-    if (b == BigInt.zero) {
-      return a;
+    while (a1 % BigInt.two == BigInt.zero) {
+      a1 = a1 ~/ BigInt.two;
+      e = e + BigInt.one;
+    }
+    BigInt s;
+    if (e % BigInt.two == BigInt.zero ||
+        n % BigInt.from(8) == BigInt.one ||
+        n % BigInt.from(8) == BigInt.from(7)) {
+      s = BigInt.one;
     } else {
-      return gcd(b, a % b);
+      s = BigInt.from(-1);
     }
+
+    if (a1 == BigInt.one) {
+      return s;
+    }
+
+    if (n % BigInt.from(4) == BigInt.from(3) &&
+        a1 % BigInt.from(4) == BigInt.from(3)) {
+      s = -s;
+    }
+
+    return s * jacobi(n % a1, a1);
   }
 
-// Returns k such that b^k = 1 (mod p)
-  BigInt order(BigInt p, BigInt b) {
-    if (gcd(p, b) != BigInt.one) {
-      print("p and b are" + "not co-prime.");
-      return BigInt.from(-1);
+  BigInt square_root_mod_prime(BigInt a, BigInt p) {
+    if (!(BigInt.zero <= a && a < p)) {
+      throw Exception(['srmp']);
+    }
+    if (!(BigInt.one < p)) {
+      throw Exception(['srmp']);
     }
 
-    // Initializing k with first
-    // odd prime number
-    BigInt k = BigInt.from(3);
-    while (true) {
-      if (pow1(b, k, p) == BigInt.one) {
-        return k;
+    if (a == BigInt.zero) {
+      return BigInt.zero;
+    }
+
+    if (p == BigInt.two) {
+      return a;
+    }
+
+    BigInt jac = jacobi(a, p);
+    if (jac == BigInt.from(-1)) {
+      throw Exception(['srmp']);
+    }
+
+    if (p % BigInt.from(4) == BigInt.from(3)) {
+      return a.modPow((p + BigInt.one) ~/ BigInt.from(4), p);
+    }
+
+    if (p % BigInt.from(8) == BigInt.from(5)) {
+      BigInt d = a.modPow((p - BigInt.one) ~/ BigInt.from(4), p);
+      if (d == BigInt.one) {
+        return a.modPow((p + BigInt.from(3)) ~/ BigInt.from(8), p);
       }
-      k += BigInt.one;
-    }
-  }
-
-// function return p - 1 (= x argument)
-// as x * 2^e, where x will be odd
-// sending e as reference because
-// updation is needed in actual e
-  BigInt convertx2e(BigInt x) {
-    z = BigInt.zero;
-    while (x % BigInt.two == BigInt.zero) {
-      x ~/= BigInt.two;
-      z += BigInt.one;
-    }
-    return x;
-  }
-
-// Main function for finding
-// the modular square root
-  BigInt STonelli(BigInt n, BigInt p) {
-    // a and p should be coprime for
-    // finding the modular square root
-    if (gcd(n, p) != BigInt.one) {
-      print("a and p are not coprime");
+      if (d == p - BigInt.one) {
+        return (BigInt.two *
+                a *
+                (a * BigInt.from(4))
+                    .modPow((p - BigInt.from(5)) ~/ BigInt.from(8), p)) %
+            p;
+      }
       return BigInt.from(-1);
     }
 
-    // If below expression return (p - 1) then modular
-    // square root is not possible
-    if (pow1(n, (p - BigInt.one) ~/ BigInt.two, p) == (p - BigInt.one)) {
-      print("no sqrt possible");
-      return BigInt.from(-1);
-    }
-
-    // expressing p - 1, in terms of
-    // s * 2^e, where s is odd number
-    BigInt s, e;
-    s = convertx2e(p - BigInt.one);
-    e = z;
-
-    // finding smallest q such that q ^ ((p - 1) / 2)
-    // (mod p) = p - 1
-    BigInt q;
-    for (q = BigInt.two;; q += BigInt.one) {
-      // q - 1 is in place of (-1 % p)
-      if (pow1(q, (p - BigInt.one) ~/ BigInt.two, p) == (p - BigInt.one)) break;
-    }
-
-    // Initializing variable x, b and g
-    BigInt x = pow1(n, (s + BigInt.one) ~/ BigInt.two, p);
-    BigInt b = pow1(n, s, p);
-    BigInt g = pow1(q, s, p);
-
-    int r = e.toInt();
-
-    // keep looping until b
-    // become 1 or m becomes 0
-    while (true) {
-      int m;
-      for (m = 0; m < r; m++) {
-        if (order(p, b) == BigInt.from(-1)) {
-          return BigInt.from(-1);
+    BigInt rangeTop = p;
+    for (BigInt b = BigInt.two; b < rangeTop; b += BigInt.one) {
+      if (jacobi(b * b - BigInt.from(4) * a, p) == BigInt.from(-1)) {
+        List<BigInt> f = [a, -b, BigInt.one];
+        List<BigInt> ff = polynomial_exp_mod(
+            [BigInt.zero, BigInt.one], (p + BigInt.one) ~/ BigInt.two, f, p);
+        if (!(ff[1] == 0)) {
+          throw Exception(['srmp']);
         }
+        return ff[0];
+      }
+    }
+    throw Exception(['srmp']);
+  }
 
-        // finding m such that b^ (2^m) = 1
-        if (order(p, b) == BigInt.two.pow(m)) {
-          break;
+  List<BigInt> polynomial_reduce_mod(
+      List<BigInt> poly, List<BigInt> polymod, BigInt p) {
+    assert(polymod[-1] == BigInt.one);
+
+    assert(polymod.length > 1);
+
+    while (poly.length >= polymod.length) {
+      if (poly[-1] != BigInt.zero) {
+        for (int i = 2; i < polymod.length + 1; i++) {
+          poly[-i] = (poly[poly.length - i] -
+                  poly[poly.length - 1] * polymod[poly.length - i]) %
+              p;
         }
       }
-      if (m == 0) {
-        return x;
-      }
-
-      // updating value of x, g and b
-      // according to algorithm
-      print(r);
-      print(m);
-      x = (x * pow1(g, BigInt.two.pow(r - m - 1), p)) % p;
-      g = pow1(g, BigInt.two.pow(r - m), p);
-      b = (b * g) % p;
-
-      if (b == BigInt.one) {
-        return x;
-      }
-      r = m;
+      poly = poly.sublist(0, poly.length - 1);
     }
+    return poly;
+  }
+
+  List<BigInt> polynomial_multiply_mod(
+      List<BigInt> m1, List<BigInt> m2, List<BigInt> polymod, BigInt p) {
+    List<BigInt> prod =
+        List<BigInt>.filled((m1.length + m2.length - 1), BigInt.zero);
+
+    for (int i = 0; i < m1.length; i++) {
+      for (int j = 0; j < m2.length; j++) {
+        prod[i + j] = (prod[i + j] + m1[i] * m2[j]) % p;
+      }
+    }
+    return polynomial_reduce_mod(prod, polymod, p);
+  }
+
+  List<BigInt> polynomial_exp_mod(
+      List<BigInt> base, BigInt exponent, List<BigInt> polymod, BigInt p) {
+    assert(exponent < p);
+
+    if (exponent == BigInt.zero) {
+      return [BigInt.one];
+    }
+
+    List<BigInt> G = base;
+    BigInt k = exponent;
+    List<BigInt> s;
+    if (k % BigInt.two == BigInt.one) {
+      s = G;
+    } else {
+      s = [BigInt.one];
+    }
+
+    while (k > BigInt.one) {
+      k = k ~/ BigInt.two;
+      G = polynomial_multiply_mod(G, G, polymod, p);
+      if (k % BigInt.two == BigInt.one) {
+        s = polynomial_multiply_mod(G, s, polymod, p);
+      }
+    }
+
+    return s;
   }
 }
 
 main() {
   int myKeyIndex = 3;
 
-  var pub_keys = [BigInt.parse("106796914794365941029590955477759833492971384141416970831227660585732906202480"), BigInt.parse("114304045427248237391922753844651658432317111645386286827488113176123620184519"),BigInt.parse("33458576324051207416269679674752536950199746778021655933675753586286967591271"), BigInt.parse("110452076032873631184811228032808144474523286758868558423802592600237088849656"),BigInt.parse("46804842327670116306537165449763483414140501415023114236312611623426800152308"), BigInt.parse("28394170371524835401420889066968091986280308546227099562813274173403045425273"),BigInt.parse("24377684314135963055386437056785121263440998313429916493604886715720326850663"), BigInt.parse("67131131321752850075978955767222964993685734666555900262316052255566279948124"),BigInt.parse("82117509818995487259743241860708767710079459435126340981597320962770220274989"), BigInt.parse("59816992311953849230737957310615071227934350560380425803949001979119080541261"),BigInt.parse("31262064332185751030941147394147810753413234906694879978932512538379103118917"), BigInt.parse("78652305106984111195689480033254652387139284882535989326621266337726361198979"),BigInt.parse("28039700284136031125338919645451230509692227280382021112962651349129520818521"), BigInt.parse("96739830825861634089353045065910032861257946952315071323525074310462450877062"),BigInt.parse("70745726080842112939044575132193608683316940420026561904236145755609477025974"), BigInt.parse("57710171067965812919594016510756929684748493880070765920287951258527413997390"),BigInt.parse("86963971812811151621600949548013712753106316357780731627078269701451129156157"), BigInt.parse("34524435231328353566123052024798884908646409158801517574140302339021059614180"),BigInt.parse("77193071401374898105307778796101095394355907956417464722689582220953323803863"), BigInt.parse("92438610242949519264528535638847009451796913260720761249946275160816723933502")];
-  var privateKey = BigInt.parse("10847031540638908869647665033049572579030779063314235603902587403290407360655");
+  var pub_keys = [
+    BigInt.parse(
+        "106796914794365941029590955477759833492971384141416970831227660585732906202480"),
+    BigInt.parse(
+        "114304045427248237391922753844651658432317111645386286827488113176123620184519"),
+    BigInt.parse(
+        "33458576324051207416269679674752536950199746778021655933675753586286967591271"),
+    BigInt.parse(
+        "110452076032873631184811228032808144474523286758868558423802592600237088849656"),
+    BigInt.parse(
+        "46804842327670116306537165449763483414140501415023114236312611623426800152308"),
+    BigInt.parse(
+        "28394170371524835401420889066968091986280308546227099562813274173403045425273"),
+    BigInt.parse(
+        "24377684314135963055386437056785121263440998313429916493604886715720326850663"),
+    BigInt.parse(
+        "67131131321752850075978955767222964993685734666555900262316052255566279948124"),
+    BigInt.parse(
+        "82117509818995487259743241860708767710079459435126340981597320962770220274989"),
+    BigInt.parse(
+        "59816992311953849230737957310615071227934350560380425803949001979119080541261"),
+    BigInt.parse(
+        "31262064332185751030941147394147810753413234906694879978932512538379103118917"),
+    BigInt.parse(
+        "78652305106984111195689480033254652387139284882535989326621266337726361198979"),
+    BigInt.parse(
+        "28039700284136031125338919645451230509692227280382021112962651349129520818521"),
+    BigInt.parse(
+        "96739830825861634089353045065910032861257946952315071323525074310462450877062"),
+    BigInt.parse(
+        "70745726080842112939044575132193608683316940420026561904236145755609477025974"),
+    BigInt.parse(
+        "57710171067965812919594016510756929684748493880070765920287951258527413997390"),
+    BigInt.parse(
+        "86963971812811151621600949548013712753106316357780731627078269701451129156157"),
+    BigInt.parse(
+        "34524435231328353566123052024798884908646409158801517574140302339021059614180"),
+    BigInt.parse(
+        "77193071401374898105307778796101095394355907956417464722689582220953323803863"),
+    BigInt.parse(
+        "92438610242949519264528535638847009451796913260720761249946275160816723933502")
+  ];
+  var privateKey = BigInt.parse(
+      "10847031540638908869647665033049572579030779063314235603902587403290407360655");
   List<AffinePoint> pubKeyList = [];
-  for (int i = 0; i <pub_keys.length; i+=2){
-    pubKeyList.add(AffinePoint.fromXY(pub_keys[i], pub_keys[i+1]));
+  for (int i = 0; i < pub_keys.length; i += 2) {
+    pubKeyList.add(AffinePoint.fromXY(pub_keys[i], pub_keys[i + 1]));
   }
   Ring ring = Ring(pubKeyList);
   print(ring.sign("hello", myKeyIndex, privateKey));
 }
-
